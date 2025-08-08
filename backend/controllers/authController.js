0cc5571d0afd909a3c579532c52ccb6e32c714e7
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-08 13:25:09 +1000

    PMS-5 Edited error message in loginUser.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,7 +27,7 @@ const loginUser = async (req, res) => {
         if (user && (await bcrypt.compare(password, user.password))) {
             res.json({ id: user.id, name: user.name, username: user.username, token: generateToken(user.id) });
         } else {
-            res.status(401).json({ message: 'Invalid email or password' });
+            res.status(401).json({ message: 'Error: Incorrect username or password.' });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
