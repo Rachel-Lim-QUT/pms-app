743236e66925c7807c0b86410de26a7fa772a951
Rachel Lim <r20.lim@connect.qut.edu.au>
2025-08-08 17:03:25 +1000

    PMS-6 Edited error messages in getProfile. (Forgot to save earlier.)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,9 +38,8 @@ const getProfile = async (req, res) => {
     try {
       const user = await User.findById(req.user.id);
       if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'Error 404: User not found.' });
       }
-  
       res.status(200).json({
         name: user.name,
         role: user.role,
@@ -48,7 +47,7 @@ const getProfile = async (req, res) => {
         password: user.password,
       });
     } catch (error) {
-      res.status(500).json({ message: 'Server error', error: error.message });
+      res.status(500).json({ message: 'Error 500: Server error.', error: error.message });
     }
   };
 
